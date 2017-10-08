4639c0ec9288d8eba9c730155b8064da16e29cb1
Encode.X <encode.xii@gmail.com>
2017-10-07 22:44:24 -0500

    commit before merge

---
+++ src/pages/p2i-new-question/p2i-new-question.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+/**
+ * This class represents the lazy loaded P2iNewQuestionComponent.
+ */
+@Component({
+  selector: 'sd-p2i-new-question',
+  templateUrl: 'p2iNewQuestion.html',
+})
+export class P2iNewQuestionComponent { }
